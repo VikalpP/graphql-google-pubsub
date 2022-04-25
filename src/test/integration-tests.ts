db69090852f5d578edbe2477b8820f31642b8dc3
VikalpP <vikalppanseriya@gmail.com>
2022-04-25 22:43:24 +0530

    fix: integration tests

--- src/test/integration-tests.ts
+++ src/test/integration-tests.ts
@@ -1,7 +1,12 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import { mock } from 'simple-mock';
-import { parse, GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql';
+import {
+  parse,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+} from 'graphql';
 import { isAsyncIterable } from 'iterall';
 import { subscribe } from 'graphql/subscription';
 
@@ -20,28 +25,31 @@ function buildSchema(iterator) {
       fields: {
         testString: {
           type: GraphQLString,
-          resolve: function(_, args) {
+          resolve: function (_, args) {
             return 'works';
-          }
-        }
-      }
+          },
+        },
+      },
     }),
     subscription: new GraphQLObjectType({
       name: 'Subscription',
       fields: {
         testSubscription: {
           type: GraphQLString,
-          subscribe: withFilter(() => iterator, () => true),
-          resolve: root => {
+          subscribe: withFilter(
+            () => iterator,
+            () => true
+          ),
+          resolve: (root) => {
             return 'FIRST_EVENT';
-          }
-        }
-      }
-    })
+          },
+        },
+      },
+    }),
   });
 }
 
-describe('PubSubAsyncIterator', function() {
+describe('PubSubAsyncIterator', function () {
   const query = parse(`
     subscription S1 {
       testSubscription
@@ -52,34 +60,36 @@ describe('PubSubAsyncIterator', function() {
   const origIterator = pubsub.asyncIterator(FIRST_EVENT);
   const returnSpy = mock(origIterator, 'return');
   const schema = buildSchema(origIterator);
-  const results = subscribe(schema, query);
+  const results = subscribe({ schema, document: query });
 
-  it('should allow subscriptions', () =>
-    results
-      .then(async ai => {
-        // tslint:disable-next-line:no-unused-expression
-        expect(isAsyncIterable(ai)).to.be.true;
+  it('should allow subscriptions', async () => {
+    const ai = await results;
 
-        const r = ai.next();
-        await pubsub.publish(FIRST_EVENT, {});
+    // tslint:disable-next-line:no-unused-expression
+    expect(isAsyncIterable(ai)).to.be.true;
 
-        return r;
-      })
-      .then(res => {
+    if (isAsyncIterable(ai)) {
+      const res = await ai.next();
+      await pubsub.publish(FIRST_EVENT, {});
+
+      // tslint:disable-next-line:no-unused-expression
+      expect(res.value).to.be.exist;
+      if (res.value) {
         expect(res.value.data.testSubscription).to.equal('FIRST_EVENT');
-      })).timeout(6000);
+      }
+    }
+  }).timeout(6000);
 
-  it('should clear event handlers', () =>
-    results
-      .then(ai => {
-        // tslint:disable-next-line:no-unused-expression
-        expect(isAsyncIterable(ai)).to.be.true;
+  it('should clear event handlers', async () => {
+    const ai = await results;
 
-        pubsub.publish(FIRST_EVENT, {});
+    // tslint:disable-next-line:no-unused-expression
+    expect(isAsyncIterable(ai)).to.be.true;
 
-        return ai.return();
-      })
-      .then(res => {
-        expect(returnSpy.callCount).to.be.gte(1);
-      }));
+    if (isAsyncIterable(ai)) {
+      pubsub.publish(FIRST_EVENT, {});
+      await ai.return();
+      expect(returnSpy.callCount).to.equal(1);
+    }
+  });
 });
